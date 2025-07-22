d07c32d668c2e92bab390446da71d1493cb1a66a
Sandesh-bn <sandesh647bn@gmail.com>
2025-07-22 12:30:00 -0700

    add test for roman numeral conversion method

---
+++ tests/romanNumeralConversion.test.js
@@ -0,0 +1,59 @@
+const { getRomanNumeral } = require('../utils/romanNumeralConversion');
+
+describe('romanNumeralConversaion', () => {
+  test('converts 0 to empty string', () => {
+    expect(getRomanNumeral(0)).toBe('');
+  });
+
+  test('converts 1 to I', () => {
+    expect(getRomanNumeral(1)).toBe('I');
+  });
+
+  test('converts 4 to IV', () => {
+    expect(getRomanNumeral(4)).toBe('IV');
+  });
+
+  test('converts 9 to IX', () => {
+    expect(getRomanNumeral(9)).toBe('IX');
+  });
+
+  test('converts 40 to XL', () => {
+    expect(getRomanNumeral(40)).toBe('XL');
+  });
+
+  test('converts 48 to XLVIII', () => {
+    expect(getRomanNumeral(48)).toBe('XLVIII');
+  });
+
+  test('converts 90 to XC', () => {
+    expect(getRomanNumeral(90)).toBe('XC');
+  });
+
+  test('converts 99 to XCIX', () => {
+    expect(getRomanNumeral(99)).toBe('XCIX');
+  });
+
+  test('converts 3999 to MMMCMXCIX', () => {
+    expect(getRomanNumeral(3999)).toBe('MMMCMXCIX');
+  });
+
+  test('converts 2023 to MMXXIII', () => {
+    expect(getRomanNumeral(2023)).toBe('MMXXIII');
+  });
+
+  test('converts 1000 to M', () => {
+    expect(getRomanNumeral(1000)).toBe('M');
+  });
+
+  test('converts 3000 to MMM', () => {
+    expect(getRomanNumeral(3000)).toBe('MMM');
+  });
+
+  test('converts 3999 to MMMCMXCIX', () => {
+    expect(getRomanNumeral(3999)).toBe('MMMCMXCIX');
+  });
+  
+  test('converts 4000 to undefined', () => {
+    expect(getRomanNumeral(4000)).toBe("undefined");
+  });
+});
--- utils/romanNumeralConversion.js
+++ utils/romanNumeralConversion.js
@@ -1,10 +1,10 @@
 function getRomanNumeral(number){
-    let M = ["", "M", "MM", "MMM"];
-    let C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-    let X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-    let I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    let thousandthPlace = ["", "M", "MM", "MMM"];
+    let hundredthPlace = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    let tenthPlace = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    let onethPlace = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
 
-    return M[Math.floor(number / 1000)] + C[Math.floor((number % 1000) / 100)] + X[Math.floor((number % 100) / 10)] + I[number % 10];
+    return thousandthPlace[Math.floor(number / 1000)] + hundredthPlace[Math.floor((number % 1000) / 100)] + tenthPlace[Math.floor((number % 100) / 10)] + onethPlace[number % 10];
 }
 
 module.exports = { getRomanNumeral };
